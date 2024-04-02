d220a2398a55ae06a3d8c524f36d62996da8094e
WilliamSado <williamsado1004@gmail.com>
2024-04-02 21:56:31 +0800

    aosp: Add addons support'
    Change-Id: Id0038e1c58ae826dd0c3bb395020dab0edd4c900

--- config/common.mk
+++ config/common.mk
@@ -228,4 +228,10 @@ $(call inherit-product, vendor/aosp/config/rro_overlays.mk)
 # Pixel Framework
 $(call inherit-product-if-exists, vendor/pixel-framework/config.mk)
 
+# Add Prebuilt Apps from Github
+$(call inherit-product, vendor/DuckDuckGo/config.mk)
+
+# Add Prebuilt GCAM
+$(call inherit-product, vendor/MGC/config.mk)
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
